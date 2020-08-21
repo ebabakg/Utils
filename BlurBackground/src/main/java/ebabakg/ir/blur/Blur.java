2d17f90b51e6db1c306281f9650bdf37ab79485a
BABAK <babakgahremanzadeh@gmail.com>
2020-08-21 04:42:34 +0430

    Initial commit

---
+++ BlurBackground/src/main/java/ebabakg/ir/blur/Blur.java
@@ -0,0 +1,43 @@
+package ebabakg.ir.blur;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+
+import androidx.annotation.RequiresApi;
+
+public class Blur {
+    private Context context;
+    public Blur(Context context){
+        this.context = context;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public Drawable blur(int icon , float BITMAP_SCALE, float BLUR_RADIUS) {
+        Bitmap image = BitmapFactory.decodeResource(context.getResources(),icon);
+
+        int width = Math.round(image.getWidth() * BITMAP_SCALE);
+        int height = Math.round(image.getHeight() * BITMAP_SCALE);
+
+        Bitmap inputBitmap = Bitmap.createScaledBitmap(image, width, height, false);
+        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
+
+        RenderScript rs = RenderScript.create(context);
+        ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+        Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
+        Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);
+        theIntrinsic.setRadius(BLUR_RADIUS);
+        theIntrinsic.setInput(tmpIn);
+        theIntrinsic.forEach(tmpOut);
+        tmpOut.copyTo(outputBitmap);
+        return new BitmapDrawable( context.getResources(), outputBitmap );
+        // outputBitmap;
+    }
+}
