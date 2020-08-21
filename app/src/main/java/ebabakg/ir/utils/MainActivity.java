05a0c9ccf71e20b6476eafe8de78415be3da288c
BABAK <babakgahremanzadeh@gmail.com>
2020-08-21 14:30:38 +0430

    Add Bitmap and Drawable

--- app/src/main/java/ebabakg/ir/utils/MainActivity.java
+++ app/src/main/java/ebabakg/ir/utils/MainActivity.java
@@ -2,7 +2,11 @@ package ebabakg.ir.utils;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import ebabakg.ir.blur.EbabakgUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView view = findViewById(R.id.test_view);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            view.setImageBitmap(EbabakgUtils.blurBitmap(this,R.drawable.babak_bg2,.1f,9));
+        }
+
+
     }
 }
